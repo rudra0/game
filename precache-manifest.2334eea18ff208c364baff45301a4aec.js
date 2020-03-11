8635b0b2ecd82136a7151d8fc5caf06e608d232c
Rudra029 <prudra882@gmail.com>
2020-03-11 20:03:14 +0530

    Updates

---
+++ precache-manifest.2334eea18ff208c364baff45301a4aec.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "468cbe5d7e86aa63dc007dc3c8b280ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b9752ce587d2f5d81ce",
+    "url": "/static/css/2.8d09573e.chunk.css"
+  },
+  {
+    "revision": "436ef0e0ba38880ef469",
+    "url": "/static/css/main.87089525.chunk.css"
+  },
+  {
+    "revision": "4b9752ce587d2f5d81ce",
+    "url": "/static/js/2.6b8be477.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.6b8be477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "436ef0e0ba38880ef469",
+    "url": "/static/js/main.4faa9ad6.chunk.js"
+  },
+  {
+    "revision": "c8b8ac850bb1e01830e0",
+    "url": "/static/js/runtime-main.198a5222.js"
+  },
+  {
+    "revision": "483b05d9ac263eaf73a32ed62914296c",
+    "url": "/static/media/game.483b05d9.png"
+  },
+  {
+    "revision": "041fd575c705521e65e9e750c2036f12",
+    "url": "/static/media/game_dikha.041fd575.mp4"
+  },
+  {
+    "revision": "ed13c2a73fd0a6ecac83b19a1403cd3a",
+    "url": "/static/media/i1.ed13c2a7.jpg"
+  },
+  {
+    "revision": "8b90e3396df52cafebbfe65b5f6e0635",
+    "url": "/static/media/i2.8b90e339.jpg"
+  },
+  {
+    "revision": "a89d570e025c78ccc15278fea90d20d7",
+    "url": "/static/media/i3.a89d570e.jpg"
+  },
+  {
+    "revision": "8e4ef122d1b41b52303b7895db3b42eb",
+    "url": "/static/media/i5.8e4ef122.jpg"
+  },
+  {
+    "revision": "9c5af105f38f39bb03a4367215b7dc38",
+    "url": "/static/media/the.9c5af105.png"
+  }
+]);
